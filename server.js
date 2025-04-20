b4e86ace6fd102bab9f8beb83cf73d61fe875253
hassanosama5 <hassanosama085@gmail.com>
2025-04-20 14:36:21 +0200

    changes in server.js to match my .env file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 // server.js
 mongoose
-  .connect("mongodb://localhost:27017/online-event-ticketing-system")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log(" MongoDB connected");
     app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
@@ -15,14 +15,3 @@ mongoose
     console.error("❌ Connection failed:", err);
     process.exit(1);
   });
-
-// mongoose
-// .connect(process.env.MONGO_URI || LOCAL_DB)
-// .then(() => {
-//   console.log("✅ MongoDB connected");
-//   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-// })
-// .catch((err) => {
-//   console.error("❌ MongoDB connection failed:", err.message);
-//   process.exit(1); // Exit if DB connection fails
-// });
