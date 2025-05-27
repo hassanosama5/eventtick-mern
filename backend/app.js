dd577f3c523fb00d940bf552df24132666f96807
Hassan Osama <73666211+hassanosama5@users.noreply.github.com>
2025-05-27 14:48:29 +0300

    adjusted cors to allow frontend in deployment

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,22 @@ app.use(cookieParser());
 app.use(express.json());
 
 // CORS Configuration
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://eventtick-mern.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman, curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
